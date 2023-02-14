c5ce7404d97565b270e6063e9c17ecc926463a54
UmayrNordien <umayrnordien@gmail.com>
2023-02-14 15:59:18 +0200

    collab

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -5,9 +5,12 @@ We’re gonna have a separate folder for configuration.
 Let’s create config folder in the app folder, under application root folder, 
 then create db.config.js file inside that config folder with content like this: */
 // example db
+
+require('dotenv').config();
+
 module.exports = {
-    HOST: "buabqxo7kdv6yi4rqnlp-mysql.services.clever-cloud.com",
-    USER: "uczzcomwgqgakbae",
-    PASSWORD: "IbOM4ObnX6MVBq19efsy",
-    DB: "b51bw3ex34ta6hc2nboh"
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DB: process.env.DB
   };
